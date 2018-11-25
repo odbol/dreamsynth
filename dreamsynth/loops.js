76c74878f14d1d23727674b9b5d97f6b2bb7665f
odbol <git@odbol.com>
2018-11-24 21:57:40 -0800

    Added 3d models from Poly. Got basic obj loader working, except for materials/colors

--- dreamsynth/loops.js
+++ dreamsynth/loops.js
@@ -40,3 +40,63 @@ var loopFiles = {
 			debugPrint('Loaded sample ' + sampleName);
 		});
 
+
+
+
+var Boat = function (scene, assetsPath, objFile, mtlFile, radius) {
+	this.radius = radius;
+	this.orbit = 0;
+};
+
+Boat.prototype.load = function(path, objFile, mtlFile) {
+	var self = this;
+	return new Promise(function (resolve, reject) 
+	    {
+	        var mtlLoader = new THREE.MTLLoader();
+	        // mtlLoader.crossOrigin = true;
+	        mtlLoader.setPath(path);
+	        mtlLoader.load(mtlFile, function (materials) 
+	        {           
+	            materials.preload();
+	            var objLoader = new THREE.OBJLoader();
+	                objLoader.setPath(path);
+
+	                objLoader.setMaterials(materials);
+	                objLoader.load(objFile, function (obj) 
+	                {   
+	                    self.obj = obj;
+	                    resolve(self);
+	                });
+	        });
+	    });
+};
+
+Boat.prototype.update = function() {
+	this.orbit++;
+	this.obj.position.y = 0;
+	this.obj.position.x = Math.sin(this.orbit) * radius;
+	this.obj.position.z = Math.cos(this.orbit) * radius;
+};
+
+
+
+var Boats = function(scene, loops, modelFiles) {
+	var self = this;
+	self.models = [];
+	modelFiles.forEach(function(boatFile, i) {
+		var boat = new Boat(i * 50);
+		boat.load(encodeURI(boatFile.assetPath + '/'), encodeURI(boatFile.objFile), encodeURI(boatFile.mtlFile))
+			.then(function(boat) {
+				boat.update();
+				scene.add(boat.obj);
+				self.models.push(boat);
+			});
+	});
+};
+
+Boats.prototype.update = function() {
+	this.models.forEach(function (model) {
+		model.update();
+	});
+};
+
