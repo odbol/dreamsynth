2d4c422897f30e8d9fb220e0d28eba3adcb21a48
odbol <git@odbol.com>
2018-11-28 13:26:55 -0800

    Lowed volume of drum loops

--- dreamsynth/loops.js
+++ dreamsynth/loops.js
@@ -1,10 +1,21 @@
-var Loops = function(urls, onLoaded, onProgress) {
+var Loops = function(loopFiles, onLoaded, onProgress) {
+	this.loopFiles = loopFiles;
+	var urls = {};
+	for (var name in loopFiles) {
+		urls[name] = loopFiles[name].url;
+	}
+
 	this.players = new Tone.Players(urls, onLoaded)
 		.toMaster();
 };
 
 Loops.prototype.startAll = function() {
+	var self = this;
 	this.players._forEach(function(player, name) {
+		var volume = self.loopFiles[name].volume;
+		if (typeof(volume) != 'undefined') {
+			player.volume.value = volume;
+		}
 		player.loop = true;
 
 		player.sync().start(0);
@@ -15,14 +26,19 @@ Loops.prototype.startAll = function() {
 Loops.prototype.mute = function(sampleName, isMuted) {
 	this.players.get(sampleName).mute = isMuted;
 };
-Loops.prototype.toggle = function(sampleIndex) {
+Loops.prototype.getByIndex = function(sampleIndex) {
 	var i = 0;
 	for(var name in this.players._players) {
 		if (i++ == sampleIndex) {
 			var player = this.players.get(name);
-			player.mute = !player.mute;
+			return player;
 		}
 	}
+	return false;
+};
+Loops.prototype.toggle = function(sampleIndex) {
+	var player = this.getByIndex(sampleIndex);
+	player.mute = !player.mute;
 };
 
 var MODEL_ROTATION = 10;
