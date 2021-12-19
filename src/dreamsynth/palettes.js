4bc1962521234d409b0c580265afdc747dd57490
Tyler Freeman <github@odbol.com>
2021-12-19 10:16:23 -0800

    REmove gross colors

--- src/dreamsynth/palettes.js
+++ src/dreamsynth/palettes.js
@@ -3,12 +3,18 @@ var PALETTES = [
 	[
 		'#C0DC47',
 		'#D5DF68',
-		'#E8EADC',
+		// '#E8EADC',
 		'#315220',
 		'#819F37'
 	], // odbol - spider
 
-	['#D5B877', '#432A29', '#82231B', '#E27546', '#AA5335'], // red room
+	[
+		//'#D5B877', 
+		// '#432A29',
+		 '#82231B',
+		 '#E27546',
+		 '#AA5335'
+	], // red room
 	[
 		'#C869FA', 
 		'#7F2EF6', 
@@ -16,19 +22,19 @@ var PALETTES = [
 		'#2415AB', 
 		// '#0E086B'
 	], // odbol jellyfish
-	[
-		'#F5E320',
-		'#42A4ED',
-		// '#323251',
-		'#AAA5CA',
-		// '#997C65'
-	], // van gogh - sower
+	// [
+	// 	'#F5E320',
+	// 	'#42A4ED',
+	// 	// '#323251',
+	// 	'#AAA5CA',
+	// 	// '#997C65'
+	// ], // van gogh - sower
 	[
 		'#83CDBE',
-		'#5A3529',
+		// '#5A3529',
 		'#B8DBCD',
-		'#212D2B',
-		'#836A54'
+		// '#212D2B',
+		// '#836A54'
 	], // odbol - machinations
 
 	[	
@@ -38,14 +44,19 @@ var PALETTES = [
 	 	'#2095CB',
 	 	'#CD1F10'
  	], // ken
-	['#6F889B', '#BFD5EC', '#35451E', '#7D4E13', '#B87E1A'], // boris
-	['#051931', '#2D4B76', '#859AD0', '#BCB5D7', '#5272AA'], // iceland sunset
+	// ['#6F889B', '#BFD5EC', '#35451E', '#7D4E13', '#B87E1A'], // boris
+	[//'#051931', 
+		'#2D4B76',
+		'#859AD0',
+		// '#BCB5D7',
+		'#5272AA'
+	], // iceland sunset
 
 	[
 		'#FBEEB1',
 		'#ECBF74',
 		'#EECB99',
-		'#D49D3D',
+		// '#D49D3D',
 		'#E2B059'
 	] // bowher
 ];
