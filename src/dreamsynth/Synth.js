43f0954ef63853166263d8aa463ab018a2d40634
Tyler Freeman <github@odbol.com>
2022-01-11 04:43:51 -0800

    Synths: buzzsaw house pump

--- src/dreamsynth/Synth.js
+++ src/dreamsynth/Synth.js
@@ -9,28 +9,68 @@ export function Synth() {
 			synthIdx = (synthIdx + 1) % 3;
 			switch(synthIdx) {
 				case 0:
-					// Marimba
-					synth = new Tone.PolySynth(4, Tone.Synth)
-						.set(
-						{
-						    "oscillator": {
-						        "partials": [
-						            1,
-						            0,
-						            2,
-						            0,
-						            3
-						        ]
-						    },
-						    "envelope": {
-						        "attack": 0.001,
-						        "decay": 1.2,
-						        "sustain": 0,
-						        "release": 1.2
-						    }
+					// House pump
+					synth = new Tone.PolySynth(1, Tone.FMSynth)
+						.set({
+							"volume": 0,
+							"detune": 0,
+							"portamento": .5,
+							"harmonicity": 1.5999999999999994,
+							"oscillator": {
+								"partialCount": 10,
+								"partials": [
+									1,
+									1,
+									1,
+									1,
+									1,
+									1,
+									1,
+									1,
+									1,
+									1
+								],
+								"phase": -11,
+								"type": "sine10"
+							},
+							"envelope": {
+								"attack": 0.0010000000000000009,
+								"attackCurve": "linear",
+								"decay": 0.2,
+								"decayCurve": "exponential",
+								"release": 0.5,
+								"releaseCurve": "exponential",
+								"sustain": 1
+							},
+							"modulation": {
+								"partialCount": 9,
+								"partials": [
+									0.8105694651603699,
+									0,
+									-0.09006327390670776,
+									0,
+									0.03242277726531029,
+									0,
+									-0.016542233526706696,
+									0,
+									0
+								],
+								"phase": 11,
+								"type": "custom"
+							},
+							"modulationEnvelope": {
+								"attack": 0.2,
+								"attackCurve": "linear",
+								"decay": 0.01,
+								"decayCurve": "exponential",
+								"release": 0.5,
+								"releaseCurve": "exponential",
+								"sustain": 1
+							},
+							"modulationIndex": 12.22
 						}
 					).toMaster();
-					synth.volume.value = -20;
+					synth.volume.value = -10;
 					return synth;
 				case 2:
 					// Kalimba
